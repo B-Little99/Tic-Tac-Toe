471ca2d29a4eae5268e3e87c327f20c26f484479
Brandon Little <brandonlittle1999@hotmail.com>
2020-04-05 22:32:49 +0100

    Added the winning combinations array in JavaScript.

--- scripts.js
+++ scripts.js
@@ -2,6 +2,19 @@ let boardTile = document.getElementsByClassName("boardTile");
 let playerIndication = document.getElementById("playerIndicator");
 let div = document.getElementsByTagName("div");
 
+
+const winningCombinations = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+]
+
 //Play game
 //Place 
 //check win
@@ -43,7 +56,6 @@ function playerTwoTurn() {
         boardTile[i].addEventListener("click", placeO);
     }
 }
-
 
 function placeO(ev){
     let sectionId = ev.target.id;
